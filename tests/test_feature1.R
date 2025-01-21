c2eeaa93bce23cddfa4affeab43a57398ac587e6
Bret Beheim <=>
2025-01-21 08:47:27 +0100

    first commit

---
+++ tests/test_feature1.R
@@ -0,0 +1,10 @@
+
+test_that("first test", {
+  expect_true(1 == 1)
+  expect_true(2 == 2)
+})
+
+test_that("second test", {
+  expect_true(3 == 3)
+  expect_false(4 == 5)
+})
---
+++ tests/test_feature2.R
@@ -0,0 +1,9 @@
+
+test_that("another test", {
+  expect_true(TRUE)
+})
+
+test_that("a final test", {
+  expect_false(FALSE)
+})
+
